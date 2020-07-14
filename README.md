39acc7949e80cc34351a22330193b558ea0f6840
shahprog <mdshahriyaralam552@gmail.com>
2020-07-14 21:17:54 +0600

    Test

---
+++ README.md
@@ -0,0 +1,2 @@
+# Disrank
+A lib
---
+++ disrank/generator.py
@@ -0,0 +1,9 @@
+from io import BytesIO
+from PIL import Image, ImageDraw
+
+class Generator:
+    def __init__(self):
+        self.default_bg = 'link'
+        self.default_profile = 'link'
+
+    
